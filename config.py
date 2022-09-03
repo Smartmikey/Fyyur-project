832776fa6e2f54721b11859d10a5b5a359daee1c
Michael James Monday A <donsmartez@gmail.com>
2022-09-03 10:04:55 +0100

    the final edit

--- config.py
+++ config.py
@@ -8,6 +8,9 @@ DEBUG = True
 
 # Connect to the database
 
+DB_HOST = os.getenv('DB_HOST', '127.0.0.1:5432')
+DB_USER = os.getenv('DB_USER', 'postgres')
+DB_PASSWORD = os.getenv('DB_PASSWORD', 'postgres')
+DB_NAME = os.getenv('DB_NAME', 'fyyur')
 
-# TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres@localhost:5432/fyyur'
+DB_PATH = 'postgresql://{}:{}@{}/{}'.format(DB_USER, DB_PASSWORD, DB_HOST, DB_NAME)
